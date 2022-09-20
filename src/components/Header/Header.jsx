9b0d6fb099c10749c0f37dabda588bc431df44a2
Elem <elem15@yandex.ru>
2022-09-20 19:00:15 +0300

    feat: add time of day

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import logo from '../../logo.svg';
+import './Header.css'
 
 const Header = () => (
   <header className="App-header">
     <img src={logo} className="App-logo" alt="logo" />
+    <nav className='nav-bar'>nav bar</nav>
   </header>
 )
 
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,14 +1,34 @@
 import React from 'react';
-import './Main.css'
+import './Main.css';
 
-const Main = () => (
-    <main>
-        <aside>Main aside</aside>
-        <section>
-            <h1>Main header</h1>
-            <p>Main content</p>
-        </section>    
-    </main>
-)
+const Main = () => {
+    const name = 'Mikhail';
+    const hours = (new Date()).getHours();
+    const timeOfDay = 
+        hours < 12 
+        ? 'morning' 
+        : hours < 19
+        ? 'afternoon'
+        : 'night';
+    return (
+        <main>
+            <aside>
+                <div>Good {timeOfDay}</div>
+                <div>{name}</div>
+            </aside>
+            <section>
+                <h1>Main header</h1>
+                <div>
+                    <input type="checkbox" />
+                    <p>checkbox placeholder</p>
+                    <input type="checkbox" />
+                    <p>checkbox placeholder</p>
+                    <input type="checkbox" />
+                    <p>checkbox placeholder</p>
+                </div>
+            </section>
+        </main>
+    )
+}
 
 export default Main;
